73e502cbb9d63c55939cb8bcbe579b43488886a1
alasalle <angela.lasalle91@gmail.com>
2019-02-28 12:56:18 -0500

    set up blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `WEBPT2 Lambda Labs Blog`,
+    author: `Angela La Salle`,
+    description: `A GatsbyJS blog documenting my experiences and work during Lambda Labs.`,
+    siteUrl: `https://alasalle-lambda-labs-blog.netlify.com`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `webpt2`,
     },
   },
   plugins: [
@@ -50,15 +50,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-135361217-1`,
       },
     },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Lambda Labs Blog`,
+        short_name: `Labs-Blog`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
